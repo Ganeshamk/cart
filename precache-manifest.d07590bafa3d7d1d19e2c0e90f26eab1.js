2b5ca3b1e9d48b06091d6c97ba629e379d8c1b2a
ganesh M K <ganesh.mk@medibox.in>
2020-09-06 01:36:48 +0530

    Updates

---
+++ precache-manifest.d07590bafa3d7d1d19e2c0e90f26eab1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f027c0895f0c607e131d63770ccd38e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e57839e71d84be46c1e",
+    "url": "/static/css/2.dc17f35a.chunk.css"
+  },
+  {
+    "revision": "5e4429a735a5330915a6",
+    "url": "/static/css/main.7c04b90d.chunk.css"
+  },
+  {
+    "revision": "2e57839e71d84be46c1e",
+    "url": "/static/js/2.2ab7c972.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2ab7c972.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e4429a735a5330915a6",
+    "url": "/static/js/main.ea18f2f1.chunk.js"
+  },
+  {
+    "revision": "c828dfd182e4d1f4b13a",
+    "url": "/static/js/runtime-main.59e396fc.js"
+  }
+]);
